87feb72eb041aa73ee3c8d65123fbe982ae8372b
Rui Leite <leite@utfpr.edu.br>
2020-10-18 14:30:21 -0300

    Introduz conteúdo inicial da HomeComponent e seu estilo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    HomeComponent,
     AppComponent
   ],
   imports: [
