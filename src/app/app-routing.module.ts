a8a23e0f0f6a7e423daeece270b2790f1e8d0d3e
Rui Leite <leite@utfpr.edu.br>
2020-10-18 16:41:24 -0300

    Introduz barra de navegação de feeds.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { AuthComponent } from './components/auth/auth.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,16 @@ const routes: Routes = [
   },
   {
     path: 'home',
+    children: [
+      {
+        path: 'global-feed',
+        component: ArticleListComponent
+      },
+      {
+        path: 'my-feed',
+        component: ArticleListComponent
+      }
+    ],
     component: HomeComponent
   },
   {
