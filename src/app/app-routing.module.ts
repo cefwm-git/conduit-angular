65fde0b696a133c589a859907d1240ef9f000a8f
Rui Leite <leite@utfpr.edu.br>
2020-10-18 15:00:21 -0300

    Configuração inicial de rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { AuthComponent } from './components/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    component: AuthComponent
+  },
+  {
+    path: 'login',
+    component: AuthComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
